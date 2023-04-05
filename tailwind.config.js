41b17500d5092ebd9917306f45d90c7e377d5acc
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-05 18:14:06 +0200

    add tailwind to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
 
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
