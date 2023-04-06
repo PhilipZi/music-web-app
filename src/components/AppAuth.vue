7025291c49b8296d15f78c7afbe2f311a7af5506
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-06 14:53:47 +0200

    add age validation in AppAuth component

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -95,19 +95,23 @@
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
+                name="email"
                 type="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
                 type="number"
+                name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <ErrorMessage class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
@@ -167,8 +171,8 @@ export default {
       tab: 'login',
       schema: {
         name: 'required|min:3|max:100|alpha_spaces',
-        email: '',
-        age: '',
+        email: 'required|min:3|max:100|email',
+        age: 'required|min_value:18|max_value:130',
         password: '',
         confirm_password: '',
         country: '',
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,13 @@
 import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
-import { required, min, max, alpha_spaces as alphaSpaces } from '@vee-validate/rules'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  min_value as minVal,
+  max_value as maxVal
+} from '@vee-validate/rules'
 
 export default {
   install(app) {
@@ -11,5 +19,8 @@ export default {
     defineRule('min', min)
     defineRule('max', max)
     defineRule('alpha_spaces', alphaSpaces)
+    defineRule('email', email)
+    defineRule('min_value', minVal)
+    defineRule('max_value', maxVal)
   }
 }
