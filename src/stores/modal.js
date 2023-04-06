0785d918ee731df69bd9276713b6b0b4fc2f46b8
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-06 13:06:09 +0200

    add getters fn hiddenClass to stores add getter to appauth component and use state and fn to hide and show modal

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -3,5 +3,10 @@ import { defineStore } from 'pinia'
 export default defineStore('modal', {
   state: () => ({
     isOpen: false
-  })
+  }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
 })
