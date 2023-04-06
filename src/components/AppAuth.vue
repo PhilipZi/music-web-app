49a4db00dc3b18d08ef3ee43d5d2fec5a3ea14a5
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-06 17:01:55 +0200

    add alerts for submitting authform

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -80,6 +80,13 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div
+            class="text-whit text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
@@ -171,6 +178,7 @@
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -201,7 +209,11 @@ export default {
       },
       userData: {
         country: 'USA'
-      }
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created.'
     }
   },
   computed: {
@@ -212,6 +224,13 @@ export default {
   },
   methods: {
     register(values) {
+      this.reg_show_alert = true
+      this.reg_in_submission = true
+      this.reg_alert_variant = 'bg-blue-500'
+      this.reg_alert_msg = 'Please wait! Your account is being created.'
+
+      this.reg_alert_variant = 'bg-green-500'
+      this.reg_alert_msg = 'Success! Your account has been created.'
       console.log(values)
     }
   }
