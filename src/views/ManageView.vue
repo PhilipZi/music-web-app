ba9e182367ac0297f89e6de580225a9d6528015b
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-10 14:13:35 +0200

    loop to all catched songs in manageView component

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -12,7 +12,7 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <composition-item />
+            <composition-item v-for="song in songs" :key="song.docID" />
           </div>
         </div>
       </div>
