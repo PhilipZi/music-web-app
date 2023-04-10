ffaecf479a02c652507934c9976c4fe23a7ffabd
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-10 14:37:41 +0200

    add validation to edit input fields via schema

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -13,26 +13,30 @@
       </button>
     </div>
     <div v-show="showForm">
-      <form>
+      <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input
+          <vee-field
             type="text"
+            name="modified_name"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input
+          <vee-field
             type="text"
+            name="genre"
             class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
         <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -48,7 +52,16 @@ export default {
   },
   data() {
     return {
-      showForm: false
+      showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces'
+      }
+    }
+  },
+  methods: {
+    edit() {
+      console.log('songs edited')
     }
   }
 }
