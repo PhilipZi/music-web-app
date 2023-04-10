8729a09c31bef1e88b2cef395a9aa29eed68a0d3
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-10 15:07:02 +0200

    add method in compositionitem comp for ui feedback and updateSong list via props from manageview comp via index and value

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -13,6 +13,13 @@
       </button>
     </div>
     <div v-show="showForm">
+      <div
+        class="text-white text-center font-bold p-4 mb-4"
+        v-if="show_alert"
+        :class="alert_variant"
+      >
+        {{ alert_message }}
+      </div>
       <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
@@ -34,19 +41,42 @@
           />
           <ErrorMessage class="text-red-600" name="genre" />
         </div>
-        <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
-        <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">Go Back</button>
+        <button
+          type="submit"
+          class="py-1.5 px-3 rounded text-white bg-green-600"
+          :disabled="in_submission"
+        >
+          Submit
+        </button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          :disabled="in_submission"
+          @click.prevent="showForm = false"
+        >
+          Go Back
+        </button>
       </vee-form>
     </div>
   </div>
 </template>
 
 <script>
+import { songsCollection } from '../includes/firebase'
+
 export default {
   name: 'CompositionItem',
   props: {
     song: {
       type: Object,
+      required: true
+    },
+    updateSong: {
+      type: Function,
+      required: true
+    },
+    index: {
+      type: Number,
       required: true
     }
   },
@@ -56,12 +86,34 @@ export default {
       schema: {
         modified_name: 'required',
         genre: 'alpha_spaces'
-      }
+      },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: 'bg-blue-500',
+      alert_message: 'Please wait! Updating song info.'
     }
   },
   methods: {
-    edit() {
-      console.log('songs edited')
+    async edit(values) {
+      this.in_submission = true
+      this.show_alert = true
+      this.alert_variant = 'bg-blue-500'
+      this.alert_message = 'Please wait! Updating song info.'
+
+      try {
+        await songsCollection.doc(this.song.docID).update(values)
+      } catch (error) {
+        this.in_submission = false
+        this.alert_variant = 'bg-red-500'
+        this.alert_message = 'Something went wrong! Try again later.'
+        return
+      }
+
+      this.updateSong(this.index, values)
+
+      this.in_submission = false
+      this.alert_variant = 'bg-green-500'
+      this.alert_message = 'Success!'
     }
   }
 }
--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -12,7 +12,13 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <composition-item v-for="song in songs" :key="song.docID" :song="song" />
+            <composition-item
+              v-for="(song, i) in songs"
+              :key="song.docID"
+              :song="song"
+              :updateSong="updateSong"
+              :index="i"
+            />
           </div>
         </div>
       </div>
@@ -45,6 +51,12 @@ export default {
 
       this.songs.push(song)
     })
+  },
+  methods: {
+    updateSong(i, values) {
+      this.songs[i].modified_name = values.modified_name
+      this.songs[i].genre = values.genre
+    }
   }
 }
 // beforRouteLeave(to, from, next) {
