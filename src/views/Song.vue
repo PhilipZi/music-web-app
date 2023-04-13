30fe6eb052a125382eb27f3b3e52f91500b6ad63
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-13 16:42:33 +0200

    add comment count in addComment fn to count comments made for song

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -21,7 +21,7 @@
   <section class="container mx-auto mt-6">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.comment_count }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -142,6 +142,11 @@ export default {
       }
 
       await commentsCollection.add(comment)
+
+      this.song.comment_count += 1
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count
+      })
 
       this.getComments()
 
