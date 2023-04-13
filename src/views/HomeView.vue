b9d1a867a0a61f7b88d15234ba3a020b753633af
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-13 23:35:33 +0200

    add icon directive for headphone icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,9 +22,8 @@
     </section>
     <section class="container mx-auto">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon>
           <span class="card-title">Songs</span>
-          <i class="fa fa-headphones-alt float-right text-green-400 text-2xl"></i>
         </div>
         <ol id="playlist">
           <SongItem v-for="song in songs" :key="song.docID" :song="song" />
