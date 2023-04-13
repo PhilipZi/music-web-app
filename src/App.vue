3111f5effba9434f348037eaa51acf7fbb3bb889
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-13 23:03:18 +0200

    add transition routes to fade routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <app-header />
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
   <!-- Player -->
   <app-player />
   <!-- AppAuth Modal -->
@@ -29,3 +33,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
