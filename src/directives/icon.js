955944f236204a6802c622a79fa44143685be129
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-13 23:50:25 +0200

    add binded  modifier to icon directive

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,5 +1,21 @@
 export default {
-  beforeMount(el) {
-    el.innerHTML += '<i class="fa fa-headphones-alt float-right text-green-400 text-2xl"></i>'
+  beforeMount(el, binding) {
+    let iconClass = `fa fa-${binding.value} text-2xl`
+
+    if (binding.arg === 'full') {
+      iconClass = binding.value
+    }
+
+    if (binding.modifiers.right) {
+      iconClass += ' float-right'
+    }
+
+    if (binding.modifiers.yellow) {
+      iconClass += ' text-amber-300'
+    } else {
+      iconClass += ' text-green-400'
+    }
+
+    el.innerHTML += `<i class="${iconClass}"></i>`
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,10 @@
     </section>
     <section class="container mx-auto">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon>
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon.right.yellow="'headphones-alt'"
+        >
           <span class="card-title">Songs</span>
         </div>
         <ol id="playlist">
