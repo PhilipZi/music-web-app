d8615628a306dba6c15c5612e6ef187f405f8e15
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-13 13:43:50 +0200

    add sortedComments fn to computed and add to select element to show latest or oldest comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -50,6 +50,7 @@
         </vee-form>
 
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -62,7 +63,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docID"
     >
       <div class="mb-5">
@@ -94,11 +95,21 @@ export default {
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
-      comments: []
+      comments: [],
+      sort: '1'
     }
   },
   computed: {
-    ...mapState(useUserStore, ['userLoggedIn'])
+    ...mapState(useUserStore, ['userLoggedIn']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted)
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted)
+      })
+    }
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get()
