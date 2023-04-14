4a226df3484a692206a632d729614c75e7d9717d
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-14 16:46:21 +0200

    add en.json data to song comp

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -23,7 +23,9 @@
     <section class="container mx-auto mt-6" id="comments">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-          <span class="card-title">Comments ({{ song.comment_count }})</span>
+          <span class="card-title">{{
+            $tc('song.comment_count', song.comment_count, { count: song.comment_count })
+          }}</span>
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
