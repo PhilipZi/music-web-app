03915b6c8fcf47ab8449d6d22f2c60499b84c2a3
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-14 16:30:59 +0200

    add de variant for home.listen comp

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import en from '../locales/en.json'
+import de from '../locales/de.json'
 
 export default createI18n({
-  local: 'en',
+  locale: 'en',
   fallbackLocale: 'en',
   messages: {
-    en
+    en,
+    de
   }
 })
