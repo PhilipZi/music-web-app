e8b3c9042e6227f39d83befb2b5c54b8e55be17b
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-14 21:42:53 +0200

    create manifest file add manifest to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,17 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        name: 'Music App',
+        theme_color: '#ff5e3a',
+        icons: [
+          {
+            src: '/assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
