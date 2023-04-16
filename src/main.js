3e288902f5591ce2b3d99285fd220a1fe3b2268a
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-16 12:21:23 +0200

    create global component button and use in AboutView compo

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { auth } from './includes/firebase'
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
 import { registerSW } from 'virtual:pwa-register'
-import { GlobalComponents } from './includes/_globals'
+import GlobalComponents from './includes/_globals'
 
 import './assets/base.css'
 import './assets/main.css'
