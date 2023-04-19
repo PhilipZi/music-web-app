00b6812dca67474c7a583b2d7ec8abb513ad7e49
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-19 23:07:22 +0200

    add dev-dist to git ignore

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
-// import { visualizer } from 'rollup-plugin-visualizer'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -28,8 +27,8 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,jpg,}']
       }
-    })
-    // visualizer({ open: true })
+    }),
+    visualizer({ open: true })
   ],
   resolve: {
     alias: {
